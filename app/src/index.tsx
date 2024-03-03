c3bd2487d22406517dfce2d163fc26b6e46e4344
daniel o <danielo.dfxi@gmail.com>
2024-03-02 20:17:04 -0500

    first update of prod (#9)
    * Create test1
    
    testing as user unit9a
    
    * Unit9a patch 2 (#4)
    
    * Create CODEOWNERS
    
    * Create index.tsx
    
    * Update CODEOWNERS
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update index.tsx
    
    * Update index.tsx
    
    * Update index.tsx
    
    ---------
    
    Co-authored-by: daniel o <danielo.dfxi@gmail.com>
    
    * Create test.ext (#3)
    
    prs is required
    
    Co-authored-by: daniel o <danielo.dfxi@gmail.com>
    
    * Unit9a test1 patch 2 (#6)
    
    * Create CODEOWNERS
    
    * Create index.tsx
    
    * Update CODEOWNERS
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update index.tsx
    
    ---------
    
    Co-authored-by: daniel o <danielo.dfxi@gmail.com>
    
    * Src test (#7)
    
    * Create CODEOWNERS
    
    * Create index.tsx
    
    * Update CODEOWNERS
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update index.tsx
    
    ---------
    
    Co-authored-by: daniel o <danielo.dfxi@gmail.com>
    
    * Update index.tsx (#8)
    
    ---------
    
    Co-authored-by: unit9a-test1 <161925022+unit9a-test1@users.noreply.github.com>

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,4 +1,16 @@
 // index file
 
+// test 2 change
 
-// test 6 out of chronological order PR
+// test 3 - pr notice to tran failed
+  
+// test 4 change
+
+
+// test 5 - dummy account
+
+// test 6
+
+// test 7- expecting pr notice to me and tran
+
+// test 8 - out of chronological order PR -- (was  test 6) this did trigger appropriate merge confilct when updating PROD
